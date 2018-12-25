6cb7b8225f9dfe589a852e39ab78428612e04928
Dilpreet Singh <dilpreetsinghwaraich@gmail.com>
2018-12-25 11:18:02 +0530

    update country state city setup 25-12-2018

--- resources/views/Profile/Profile.blade.php
+++ resources/views/Profile/Profile.blade.php
@@ -41,9 +41,9 @@
 			?>
 		</p>
 		<p class="profile-address-line-one"><span>Address</span><?php echo $profile->address; ?></p>
-		<p class="profile-city"><span>City</span><?php echo $profile->city; ?></p>
-		<p class="profile-state"><span>State</span><?php echo $profile->state; ?></p>
-		<p class="profile-country"><span>Country</span><?php echo $profile->country. (!empty($profile->postal_code)?'('.$profile->postal_code.')':''); ?></p>
+		<p class="profile-city"><span>City</span><?php echo Helper::getCity($profile->city); ?></p>
+		<p class="profile-state"><span>State</span><?php echo Helper::getState($profile->state); ?></p>
+		<p class="profile-country"><span>Country</span><?php echo Helper::getCountry($profile->country). (!empty($profile->postal_code)?'('.$profile->postal_code.')':''); ?></p>
 		<a class="profile-edit-btn" href="<?php echo url('/edit/profile') ?>" data-url="<?php echo url('edit/profile') ?>">Edit Profile</a>
 	</div>
 </div>                  
